eead302ef4d158d73ad0c3c93e912b18ebb80981
Jarthesecond2 <janis.kaldmaa@voco.ee>
2025-11-21 14:59:58 +0200

    Create project basic files

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongodb = require('mongodb').MongoClient
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+
+connectToDB = async (connectionString) => {
+    try {
+        const client = await mongodb.connect(connectionString);
+        db = client.db();
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error("Failed to connect to MongoDB", err);
+    }
+    return db;
+}
+
+const user = process.env.MONGO_USERNAME
+const password = process.env.MONGO_PASSWORD
+const host = process.env.MONGO_HOST
+const port = process.env.MONGO_PORT
+const database = process.env.MONGO_DATABASE
+let db, connectionString = `mongodb://${user}:${password}@${host}:${port}/${database}`
+db = connectToDB(connectionString);
+
+app.listen(3012, () => {
+    console.log('Server is running on port 3012')
+})
